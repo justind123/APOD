4c4efa13603b897bcd8a18628becdda3b3a2fe96
Justin <justinduross@email.arizona.edu>
2023-11-26 09:25:38 -0800

    added emails.txt for list of receivers

--- apod.py
+++ apod.py
@@ -31,42 +31,44 @@ file.close()
 subject = "NASA APOD " + data["date"]
 body = data["explanation"]
 sender_email = "justindurossdev@gmail.com"
-receiver_email = "justinduross@gmail.com"
+email_file = open('emails.txt', 'r')
 
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-#message["Bcc"] = receiver_email  # Recommended for mass emails
+for line in email_file:
+    receiver_email = line.strip()
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    #message["Bcc"] = receiver_email  # Recommended for mass emails
 
-# Add body to email
-message.attach(MIMEText(body, "plain"))
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
 
-filename = "apodImage.jpg"  # In same directory as script
+    filename = "apodImage.jpg"  # In same directory as script
 
-# Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-# Encode file in ASCII characters to send by email
-encoders.encode_base64(part)
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
 
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
 
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
 
-# Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, app_password)
-    server.sendmail(sender_email, receiver_email, text)
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, app_password)
+        server.sendmail(sender_email, receiver_email, text)
